a0b1f95434b70f393f74b07722571f6e3c9dd948
Katherine Brock <katherine.brock@dbtlabs.com>
2022-12-21 19:16:13 +0000

    python UDF for holidays

--- models/marts/intermediate/python/int_tpch_orders_with_holidays.py
+++ models/marts/intermediate/python/int_tpch_orders_with_holidays.py
@@ -1,26 +1,28 @@
 import holidays
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import BooleanType
+
 
 def is_holiday(date_col):
     us_holidays = holidays.US()
     is_holiday = (date_col in us_holidays)
     return is_holiday
 
+holidayUDF = udf(lambda x:is_holiday(x),BooleanType())  
+
 def model(dbt, session):
     dbt.config(
         materialized = "table",
         packages = ["holidays"], #import pypi holidays package
-        create_notebook=True
+        submission_method="all_purpose_cluster",
+        cluster_id="1215-215802-worukqlx", 
+        create_notebook = True
     )
 
     orders_df = dbt.ref("stg_tpch_orders")
 
-    df = orders_df.to_pandas_on_spark() 
-
     # apply our function
-    df["is_holiday"] = df["order_date"].apply(is_holiday)
-
-    # convert back to PySpark
-    df = df.to_spark()             
+    df = orders_df.withColumn("is_holiday", holidayUDF(col("order_date")))           
 
     # return final dataset (PySpark DataFrame)
     return df
